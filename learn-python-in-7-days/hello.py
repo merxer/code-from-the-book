1b080f9fd2b8ca96cbf739cda04e73b1c5696b16
merxer <34d30y@gmail.com>
2017-06-04 15:22:58 +0700

    before format string

--- learn-python-in-7-days/hello.py
+++ learn-python-in-7-days/hello.py
@@ -1,3 +1,4 @@
-print 'a'
-print '\tHermit'
-print "\i know\,\ there are 'great'"
+print "Name", "Marks", "Age"
+print "John Doe", 80.67, "27"
+print "Bhaskar", 76.908, "27"
+print "Mohit", 56.98, "25"
