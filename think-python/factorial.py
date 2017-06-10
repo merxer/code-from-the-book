d2933b07d9781f73751e447d81744cd5f6c89424
merxer <34d30y@gmail.com>
2017-06-10 19:06:09 +0700

    good checking in factorial code

--- think-python/factorial.py
+++ think-python/factorial.py
@@ -1,7 +1,16 @@
 def factorial(n):
+    if not isinstance(n, int):
+        print('factorial is only defined for integer')
+        return None
+    elif n < 0:
+        print('factorial is only defined for integer')
+        return None
     if n == 0:
         return 1
     return n * factorial(n - 1)
 
 
 print(factorial(5))
+print(factorial(-1))
+print(factorial('hello'))
+print(factorial(1.5))
