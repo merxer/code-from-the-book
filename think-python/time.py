fde33d709cffd43e5280fd40cdcfc2c43bc412ec
merxer <34d30y@gmail.com>
2017-06-13 08:08:30 +0700

    sum times

--- think-python/time.py
+++ think-python/time.py
@@ -9,8 +9,21 @@ time.hour = 11
 time.minute = 59
 time.second = 30
 
+time2 = Time()
+time2.hour = 10
+time2.minute = 10 
+time2.second = 10
+
 def print_time(time):
   print("%.2d:%.2d:%.2d" % (time.hour, time.minute, time.second))
 
+def add_time(t1, t2):
+  sum = Time()
+  sum.hour = t1.hour + t2.hour
+  sum.minute = t1.minute + t2.minute
+  sum.second = t1.second + t2.second
+  return sum
+
 
 print_time(time)
+
