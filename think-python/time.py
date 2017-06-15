4587af37b13ee789a9bc1921250945f0820a818d
merxer <34d30y@gmail.com>
2017-06-15 19:14:08 +0700

    add time

--- think-python/time.py
+++ think-python/time.py
@@ -22,6 +22,15 @@ def add_time(t1, t2):
   sum.hour = t1.hour + t2.hour
   sum.minute = t1.minute + t2.minute
   sum.second = t1.second + t2.second
+  
+  if sum.second >= 60:
+    sum.second -= 60
+    sum.minute += 1
+
+  if sum.minute >= 60:
+    sum.minute -= 60
+    sum.hour += 1
+
   return sum
 
 
