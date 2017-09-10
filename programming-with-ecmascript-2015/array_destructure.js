e697b783fc4603127cde1c9df6c7701ca7ec9564
Administrator <admin@example.com>
2017-09-10 22:44:51 +0700

    implement in array destructuring

--- programming-with-ecmascript-2015/array_destructure.js
+++ programming-with-ecmascript-2015/array_destructure.js
@@ -1,5 +1,23 @@
-let evens = [2, 4, 6, 8, 10];
+// variable declarations
 
-let [, second, third] = evens;
+let { name } = { name : 'Lohith' };
+console.log('1. Name : ' + name);
 
-console.log(second, third);
+let [dayName] = ['Sunday'];
+console.log('2. Day Name: ' + dayName);
+
+// assignments
+[name] = ['Rasmi', 'Lohith'];
+console.log('3. Name: '+ name);
+
+// Parameter Definations
+function foo([x]) {
+    console.log('4. X: '+  x);
+}
+foo(['Adithya']);
+
+// Loops
+const evens = [2, 4, 6, 8, 10];
+for (let [index, item, next] of evens.entries()){
+    console.log(`index: ${index}, item: ${item}`);
+}
