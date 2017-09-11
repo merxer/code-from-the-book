89a0c507fc35728ac175561d5154fc82d463d27c
Administrator <admin@example.com>
2017-09-11 20:24:09 +0700

    simulate head and tail operator

--- programming-with-ecmascript-2015/rest_parameter.js
+++ programming-with-ecmascript-2015/rest_parameter.js
@@ -1,6 +1,12 @@
-function foo(bar, ...baz) {
-    return baz;
+function head_tail(head,...tail){
+    return [head, tail];
 }
 
-console.log(foo(1, 2, 3, 4, 5));
-console.log(foo());
+list = [1, 2, 3, 4, 5];
+console.log(head_tail(...list));
+
+[ head, tail ] = head_tail(...list);
+console.log(head, tail);
+
+[ head, tail ] = head_tail(...tail);
+console.log(head, tail);
