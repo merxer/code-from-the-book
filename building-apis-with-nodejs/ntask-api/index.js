209629c0c4efda1b670f6b12ebc8f361c522c902
merxer <34d30y@gmail.com>
2017-09-12 13:20:21 +0700

    seperate code by consign

--- building-apis-with-nodejs/ntask-api/index.js
+++ building-apis-with-nodejs/ntask-api/index.js
@@ -1,19 +1,13 @@
 import express from 'express';
+import consign from 'consign';
 
 const PORT = 3000;
 const app = express();
 
 app.set("json spaces", 4);
 
-app.get('/', (req, res) => res.json({status: 'NTask API'}));
-
-app.get('/tasks', (req, res) => {
-    res.json({
-        tasks: [
-            { title: 'Buy some shoes'},
-            { title: 'Fix notebook'},
-        ]
-    });
-});
+consign()
+    .include('routes')
+    .into(app);
 
 app.listen(PORT, ()=> console.log(`NTask API - Port ${PORT}`));
