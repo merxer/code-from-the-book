3ce9b8319ac272fa7a275f9b7a7367f5dfd1b285
merxer <34d30y@gmail.com>
2017-09-18 18:56:50 +0700

    boolean

--- rust-book-2/datatypes/src/main.rs
+++ rust-book-2/datatypes/src/main.rs
@@ -1,13 +1,6 @@
 fn main() {
-    let sum = 5 + 10;
+    let t = true;
+    let f: bool = false;
 
-    let difference = 95.5 - 4.3;
-
-    let product = 4 * 30;
-
-    let quotient = 56.7 / 32.2;
-
-    let reminder = 43 % 5;
-
-    print!("{} {} {} {} {}", sum, product, quotient, reminder, difference);
+    print!("{} {}", t, f);
 }
