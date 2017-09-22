226aedb0b64d218579c6b94adfb328bc00cde13e
merxer <34d30y@gmail.com>
2017-09-22 21:16:06 +0700

    functions multi parameters

--- rust-book-2/datatypes/src/main.rs
+++ rust-book-2/datatypes/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    another_function(5, 6);
+}
 
-    println!("{:?}", tup)
+fn another_function(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
 }
